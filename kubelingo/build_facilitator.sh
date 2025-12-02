21ced9d34e4dce76251cbad499237f9e9b61b61b
josephedward <josephedwardwork@gmail.com>
2025-12-01 19:24:50 -0500

    cleaning dirs

--- kubelingo/build_facilitator.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# This script builds the facilitator docker image with generated questions.
-# It should be run from the root of the CK-X project.
-echo "Building facilitator image with generated questions..."
-docker build -f kubelingo/Dockerfile.facilitator -t ckx-facilitator-generated:latest .
-echo "Successfully built ckx-facilitator-generated:latest"
